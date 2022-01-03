eec392987a4dbe24ccba2ba7f12832c25b208143
Syed Babar Tameez <stameez.bese18seecs@seecs.edu.pk>
2022-01-03 10:43:00 +0500

    Create babar_IoTproject_completed.ino

---
+++ babar_IoTproject_completed.ino
@@ -0,0 +1,93 @@
+
+#define BLYNK_PRINT Serial
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+#include <Wire.h>
+#include <Adafruit_BMP280.h>
+
+// Inserting Auth Token for Blynk App
+char auth[] = "Ql4AnWw_9e8lcwUQUx8iiEaX4lzFMQRH";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Redmi Note 10";  //Enter your WIFI Name
+char pass[] = "12abcdefg";  //Enter your WIFI Password
+
+
+#define DHTPIN 14          // D5 pin
+#define DHTTYPE DHT11     // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+
+Adafruit_BMP280 bmp; // use I2C interface
+Adafruit_Sensor *bmp_temp = bmp.getTemperatureSensor();
+Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
+
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+    if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  sensors_event_t temp_event, pressure_event;
+  bmp_temp->getEvent(&temp_event);
+  bmp_pressure->getEvent(&pressure_event);
+  
+  // Sending values to blynk app
+  Blynk.virtualWrite(V5, h);
+  Blynk.virtualWrite(V6, t);
+  Blynk.virtualWrite(V7, temp_event.temperature);
+  Blynk.virtualWrite(V8, pressure_event.pressure);
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+  
+  Blynk.begin(auth, ssid, pass);
+
+  dht.begin();
+  unsigned status;
+  status = bmp.begin(0x77);
+  if (!status) {
+    status = bmp.begin(0x76);
+    if (!status){
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    while (1) delay(10);
+  }
+  }
+    /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+                  
+  // Setup a function to be called every second
+  timer.setInterval(1000L, sendSensor);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
